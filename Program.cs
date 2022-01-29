4076982e247bafda3b1b51a38153647dbbac16d0
Minh Vo <mdvo@usf.edu>
2022-01-29 16:36:47 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+//Minh Vo
+//Tech Assignment #2 - Basic Console App
+//01/28/2022
+namespace VisualStudio2019_CSGitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am impressed by the applications of data to different fields." + 
+                "The amount of data is significantly increasing, so it is indispensable to have data-savvy employees " +
+                "who can quickly transform data into valuable insights " + 
+                "that help the business solve complex problems and make important decisions. " + 
+                "Submitted by Minh Vo");
+        }
+    }
+}
